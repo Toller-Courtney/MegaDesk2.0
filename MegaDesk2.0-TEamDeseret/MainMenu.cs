aedd0ae1549c382a718136a2215f8da506d2c791
Courtney Toller <43355629+courtneytoller@users.noreply.github.com>
2019-10-11 00:27:55 -0600

    Add project files.

---
+++ MegaDesk2.0-TEamDeseret/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk2._0_TEamDeseret
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
